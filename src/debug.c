6ffe7eb1fa49075dba793b045ccd087582943668
Brian George <bhgeorge.dev@gmail.com>
2024-04-12 08:57:52 -0500

    Allow learning pre-evo moves at move relearner.

--- src/debug.c
+++ src/debug.c
@@ -57,7 +57,7 @@ extern u8 Debug_TestTwo[];
 // Text
 static const u8 sDebugText_QuickStart[]   = _("Quick Start");
 static const u8 sDebugText_TestOne[]      = _("HOENN Dex");
-static const u8 sDebugText_TestTwo[]      = _("Shiny Charm Flag");
+static const u8 sDebugText_TestTwo[]      = _("Move Relearner");
 static const u8 sDebugText_Cancel[]       = _("Cancel");
 
 // Menu Actions
