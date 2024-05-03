eb809ae6030d986e30a149dfbc3f03c693a60a7d
Brian George <bhgeorge.dev@gmail.com>
2024-05-03 10:41:55 -0500

    Fix ground effects on surf dismount

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1645,6 +1645,7 @@ static void Task_WaitStopSurfing(u8 taskId)
         gPlayerAvatar.preventStep = FALSE;
         UnlockPlayerFieldControls();
         DestroySprite(&gSprites[playerObjEvent->fieldEffectSpriteId]);
+        playerObjEvent->triggerGroundEffectsOnMove = TRUE;
         DestroyTask(taskId);
     }
 }
