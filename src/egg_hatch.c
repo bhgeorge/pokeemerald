40118d155b2cae35acf4faf889d940b79d071518
Brian George <bhgeorge.dev@gmail.com>
2024-05-08 08:36:37 -0500

    Halved the time to hatch an egg.

--- src/egg_hatch.c
+++ src/egg_hatch.c
@@ -932,10 +932,10 @@ u8 GetEggCyclesToSubtract(void)
         {
             u8 ability = GetMonAbility(&gPlayerParty[i]);
             if (ability == ABILITY_MAGMA_ARMOR || ability == ABILITY_FLAME_BODY)
-                return 2;
+                return 4;
         }
     }
-    return 1;
+    return 2;
 }
 
 u16 CountPartyAliveNonEggMons(void)
